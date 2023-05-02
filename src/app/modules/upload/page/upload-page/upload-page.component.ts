db3a3ca0cbdd84ad5a7191a527f57a32cfab198a
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-02 12:07:58 -0500

    mockup upload

--- src/app/modules/upload/page/upload-page/upload-page.component.ts
+++ src/app/modules/upload/page/upload-page/upload-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload-page',
   templateUrl: './upload-page.component.html',
   styleUrls: ['./upload-page.component.css']
 })
-export class UploadPageComponent {
+export class UploadPageComponent  implements OnInit{
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectFile(e: any) {
+    if(e.target.files) {
+
+    }
+
+  }
 }
