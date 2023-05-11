41c6a32b661765c453217b8e5df11aa8fec4dcec
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-11 10:05:59 -0500

    implement accesLink and new style css

--- src/app/modules/main/page/main-page/main-page.component.ts
+++ src/app/modules/main/page/main-page/main-page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
 
+  mainMenu: {
+    defaultOptions: Array<any>, accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] }
+
+  ngOnInit(): void {
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Facebook',
+        icon: 'uil uil-facebook-f',
+        link: 'https://www.facebook.com/geovanny.alex.mr'
+      },
+      {
+        name: 'Github',
+        icon: 'uil uil-github',
+        link: 'https://github.com/GeovannyAlExRs'
+      },
+      {
+        name: 'Linkedin',
+        icon: 'uil uil-linkedin',
+        link: 'https://www.linkedin.com/in/geovanny-magallan-59ab5b99',
+      },
+      {
+        name: 'Geovanny',
+        icon: 'uil uil-link',
+        link: ['GeovannyAlexRs']
+      }
+    ]
+
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Cargar Foto',
+        icon: 'uil uil-image-upload',
+        router: ['/', 'upload']
+      },
+      {
+        name: 'Iniciar Scan',
+        icon: 'uil uil-capture',
+        router: ['/', 'scan'],
+      }
+    ]
+  }
 }
